d9aa16d84b088131b5b5d2329ef02de1b76d536c
stassie-lzrv <adlazareva@edu.hse.ru>
2022-09-20 14:14:31 +0300

    Added animation

--- adlazarevaPW1/ViewController.swift
+++ adlazarevaPW1/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var views: [UIView]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        for view in views {
+            view.layer.cornerRadius=10
+        }
+        
     }
-
+    @IBAction func changeColorButtonPressed(_ sender: Any) {
+        let button = sender as? UIButton
+        button?.isEnabled = false
+        var set = Set<UIColor>()
+        while set.count < views.count{
+            set.insert(
+                UIColor(
+                    red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1
+                )
+            )
+        }
+            UIView.animate(withDuration: 2,  animations: {
+                for view in self.views {
+                    view.layer.cornerRadius=16
+                    view.backgroundColor=set.popFirst()
+                }
+            }){ completion in
+                button?.isEnabled = true
+                           
+        }
+        
+    }
+    
 
 }
 
