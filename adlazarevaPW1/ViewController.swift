e44dca7f1b0f5ecef1b2cff3b59737809febb59a
stassie-lzrv <adlazareva@edu.hse.ru>
2022-09-27 19:55:47 +0300

    HEX color generation added

--- adlazarevaPW1/ViewController.swift
+++ adlazarevaPW1/ViewController.swift
@@ -7,6 +7,40 @@
 
 import UIKit
 
+extension UIColor {
+    public convenience init?(hex: String) {
+        let r, g, b, a: CGFloat
+
+        if hex.hasPrefix("#") {
+            let start = hex.index(hex.startIndex, offsetBy: 1)
+            let hexColor = String(hex[start...])
+
+            if hexColor.count == 8 {
+                let scanner = Scanner(string: hexColor)
+                var hexNumber: UInt64 = 0
+
+                if scanner.scanHexInt64(&hexNumber) {
+                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                    a = CGFloat(hexNumber & 0x000000ff) / 255
+
+                    self.init(red: r, green: g, blue: b, alpha: a)
+                    return
+                }
+            }
+        }
+
+        return nil
+    }
+}
+
+func generateString()-> String{
+    let arr = ["1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
+    return "#".appending(arr[Int.random(in:0..<15)]).appending(arr[Int.random(in:0..<15)]).appending(arr[Int.random(in:0..<15)]).appending(arr[Int.random(in:0..<15)]).appending(arr[Int.random(in:0..<15)]).appending(arr[Int.random(in:0..<15)]).appending(arr[Int.random(in:0..<15)]).appending(arr[Int.random(in:0..<15)])
+}
+
+
 class ViewController: UIViewController {
     @IBOutlet var views: [UIView]!
 
@@ -17,16 +51,17 @@ class ViewController: UIViewController {
         }
         
     }
+    
+    
+    
+    
+    
     @IBAction func changeColorButtonPressed(_ sender: Any) {
         let button = sender as? UIButton
         button?.isEnabled = false
         var set = Set<UIColor>()
         while set.count < views.count{
-            set.insert(
-                UIColor(
-                    red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1
-                )
-            )
+            set.insert(UIColor(hex: generateString())!)
         }
             UIView.animate(withDuration: 2,  animations: {
                 for view in self.views {
